3201f7281445208274c74ba47358de007d9501d2
Lucy Owen <lucywowen@gmail.com>
2017-05-02 11:36:16 -0400

    will add cleaned up code

--- code/__init__.py
+++
@@ -1,6 +0,0 @@
-import bookkeeping
-import datagen
-import file_io
-import plot
-import stats
-
--- code/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(name='superEEG',
-      version='1.1',
-      py_modules=['numpy', 'scipy', 'seaborn', 'pandas', 'pyDOE', 'mne']
-      )
