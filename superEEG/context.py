16e6a48e24f68d64470b95ef2eec94610ce5370c
andrewheusser <andrew.heusser@gmail.com>
2017-05-12 09:02:03 -0400

    added more attrs to context dict

--- superEEG/context.py
+++ superEEG/context.py
@@ -5,11 +5,19 @@ single = {
     'environment' : 'single',
     'nodes' : 1,
     'memory' : 8,
+    'job_cmd' : None,
+    'code_dir' : None,
+    'scratch_dir' : None,
+    'data_dir' : None,
 }
 cluster = {
     'environment' : 'cluster',
     'nodes' : 4,
     'memory' : 30,
+    'job_cmd' : None,
+    'code_dir' : None,
+    'scratch_dir' : None,
+    'data_dir' : None,
 }
 
 def set_context(context='single'):
