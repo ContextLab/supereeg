d31fb906a89da232a63e477ecfccdbdfacaa613c
andrewheusser <andrew.heusser@gmail.com>
2017-05-16 09:26:01 -0400

    cleaning up

--- superEEG/predict.py
+++ superEEG/predict.py
@@ -3,41 +3,6 @@
 import helpers
 from _helpers.stats import *
 
-
-# CHANGE THIS TO ACCEPT NUMPY ARRAY
-def corrmat(bo):
-    """
-    Function that calculates the average subject level correlation matrix for filename across session
-
-    Parameters
-    ----------
-    fname :  Data matrix (npz file)
-        The data to be analyzed.
-        Filename containing fields:
-            Y - time series
-            R - electrode locations
-            fname_labels - session number
-            sample_rate - sampling rate
-
-    Returns
-    ----------
-    results: 2D ndarray(len(R_subj)xlen(R_subj)) matrix
-        The average correlation matrix across sessions
-
-
-    """
-    def aggregate(prev, next):
-        return np.sum(np.concatenate((prev[:, :, np.newaxis], next[:, :, np.newaxis]), axis=2), axis=2)
-
-    def zcorr(x):
-        return r2z(1 - squareform(pdist(x.T, 'correlation')))
-
-    summed_zcorrs = apply_by_file_index(bo, zcorr, aggregate)
-    n = n_files(bo)
-
-    return z2r(summed_zcorrs / n)
-
-
 def predict(bo, model=None):
     """
     Takes a brain object and a 'full' covariance model, fills in all
