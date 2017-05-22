fc68ebf0225d3806b4deb9d5b3ed32602bb7e2b0
andrewheusser <andrew.heusser@gmail.com>
2017-05-22 11:34:23 -0400

    updated load API

--- superEEG/load.py
+++ superEEG/load.py
@@ -1,24 +1,51 @@
-import numpy as np
 import os
+import sys
+import pickle
+import numpy as np
 from .brain import Brain
 from .model import Model
 from ._helpers.stats import tal2mni
 
-def load_example_data():
+def load(dataset):
+    """
+    Load example data
 
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/../superEEG/data/BW001.npz', 'rb') as handle:
-        f = np.load(handle)
-        data = f['Y']
-        sample_rate = f['samplerate']
-        sessions = f['fname_labels']
-        locs = tal2mni(f['R'])
+    Parameters
+    ----------
+    dataset : string
+        The name of the example data or a filepath
 
-    return Brain(data=data, locs=locs, sessions=sessions, sample_rate=sample_rate)
+    Returns
+    ----------
+    data : any
+        Example data
 
-def load_example_model():
+    """
+    if sys.version_info[0]==3:
+        pickle_options = {
+            'encoding' : 'latin1'
+        }
+    else:
+        pickle_options = {}
 
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/../superEEG/data/R_small_MNI.npy', 'rb') as handle:
-        locs = np.load(handle)
-        data = np.random.rand(len(locs), len(locs))
+    # if dataset is 'example_data':
+    #     fileid = '0B7Ycm4aSYdPPREJrZ2stdHBFdjg'
+    #     url = 'https://docs.google.com/uc?export=download&id=' + fileid
+    #     data = pickle.loads(requests.get(url, stream=True).content, **pickle_options)
 
-    return Model(data=data, locs=locs, n_subs=67)
+    if dataset is 'example_data':
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/../superEEG/data/BW001.npz', 'rb') as handle:
+            f = np.load(handle)
+            data = f['Y']
+            sample_rate = f['samplerate']
+            sessions = f['fname_labels']
+            locs = tal2mni(f['R'])
+
+        return Brain(data=data, locs=locs, sessions=sessions, sample_rate=sample_rate)
+
+    elif dataset is 'example_model':
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/../superEEG/data/R_small_MNI.npy', 'rb') as handle:
+            locs = np.load(handle)
+            data = np.random.rand(len(locs), len(locs))
+
+        return Model(data=data, locs=locs, n_subs=67)
