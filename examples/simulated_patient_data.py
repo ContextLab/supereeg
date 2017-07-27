eb0c762cf09282cae7395601684b5982159186c9
Lucy Owen <lucywowen@gmail.com>
2017-07-27 16:10:07 -0400

    bypass predict function and expanded the model.  reconstruction based on indexing model with 50 patients

--- examples/simulated_patient_data.py
+++ examples/simulated_patient_data.py
@@ -30,7 +30,7 @@ import sklearn
 n_samples = 1000
 
 # n_electrodes - number of electrodes for reconstructed patient - need to loop over 5:5:130
-n_elecs =169
+n_elecs =[10, 169, 85]
 
 # m_patients - number of patients in the model - need to loop over 10:10:50
 m_patients = 50
@@ -72,39 +72,98 @@ patients = np.random.choice(range(50), m_patients, replace=False)
 # initiate dataframe
 d = []
 
-for i in patients:
+# to hold one one patient at a time:
+# for i in patients:
+#
+#
+#     p_n_elecs = np.sort(np.random.choice(range(len(locs)), n_elecs, replace=False))
+#
+#     with open(os.path.join(synth_dir, 'synthetic_'+ str(i).rjust(2, '0') + '.bo'), 'rb') as handle:
+#         bo_actual = pickle.load(handle)
+#         bo_sub = se.Brain(data=bo_actual.data.loc[:, p_n_elecs],locs= bo_actual.locs.loc[p_n_elecs])
+#
+#
+#     unknown_locs = locs.drop(p_n_elecs)
+#     unknown_inds = unknown_locs.index.values
+#
+#     # create model from every other patient
+#     model_patients = [p for p in patients if p != i]
+#     for m in model_patients:
+#
+#         p_m_elecs = np.sort(np.random.choice(range(len(locs)), m_elecs, replace=False))
+#
+#         #p_m_elecs = np.random.choice(list(unknown_inds), m_elecs, replace=False)
+#
+#         with open(os.path.join(synth_dir, 'synthetic_' + str(m).rjust(2, '0') + '.bo'), 'rb') as handle:
+#             bo = pickle.load(handle)
+#             #model_data.append(se.Brain(data=bo.data.loc[:, unknown_inds], locs=bo.locs.loc[unknown_inds]))
+#             model_data.append(se.Brain(data=bo.data.loc[:, p_m_elecs], locs=bo.locs.loc[p_m_elecs]))
+#
+#     #model = se.Model(data=model_data, locs=bo_actual.locs.loc[unknown_inds])
+#
+#     model = se.Model(data=model_data, locs=locs)
+#     reconstructed = model.predict(bo_sub)
+#
+#     new_pd = bo_actual.data.loc[:, unknown_inds]
+#     actual = (new_pd - new_pd.mean()) / new_pd.std()
+#     predicted = reconstructed.data.loc[:, unknown_inds]
+#     corr = np.mean(np.diag(pd.concat([actual, predicted], axis=1, keys=['actual', 'predicted']).corr().loc['actual', 'predicted']))
+
+for n in n_elecs:
+
+    ## to create a model from all 50 simulated patients
+    for i in patients:
+
+        p_n_elecs = np.sort(np.random.choice(range(len(locs)), n, replace=False))
+
+        with open(os.path.join(synth_dir, 'synthetic_'+ str(i).rjust(2, '0') + '.bo'), 'rb') as handle:
+            bo_actual = pickle.load(handle)
+            bo_sub = se.Brain(data=bo_actual.data.loc[:, p_n_elecs],locs= bo_actual.locs.loc[p_n_elecs])
+
+        if not os.path.isfile('model_170.mo'):
+            # create model from every patient
+            model_patients = [p for p in patients]
+            for m in model_patients:
+
+                with open(os.path.join(synth_dir, 'synthetic_' + str(m).rjust(2, '0') + '.bo'), 'rb') as handle:
+                    bo = pickle.load(handle)
+                    #model_data.append(se.Brain(data=bo.data.loc[:, unknown_inds], locs=bo.locs.loc[unknown_inds]))
+                    model_data.append(se.Brain(data=bo.data, locs=bo.locs))
+
+            model = se.Model(data=model_data, locs=locs)
+
+            with open('model_170', 'wb') as h:
+                pickle.dump(model, h)
+
+        with open('model_170', 'rb') as a:
+            mo = pickle.load(a)
+
+        def recon_no_expand(bo_sub, mo):
+            """
+            """
+            model = z2r(np.divide(mo.numerator, mo.denominator))
+            model[np.eye(model.shape[0]) == 1] = 1
+            known_locs = bo_sub.locs
+            known_inds = bo_sub.locs.index.values
+            unknown_locs = mo.locs.drop(known_inds)
+            unknown_inds = unknown_locs.index.values
+            Kba = model[unknown_inds, :][:, known_inds]
+            Kaa = model[:,known_inds][known_inds,:]
+            Y = zscore(bo_sub.get_data())
+            return np.squeeze(np.dot(np.dot(Kba, np.linalg.pinv(Kaa)), Y.T).T)
+
+        predicted = recon_no_expand(bo_sub, mo)
+
+        unknown_locs = locs.drop(p_n_elecs)
+        unknown_inds = unknown_locs.index.values
+
+        new_pd = bo_actual.data.loc[:, unknown_inds]
+        actual = (new_pd - new_pd.mean()) / new_pd.std()
+        #sb.jointplot(actual.as_matrix().flatten(), predicted)
+        corr = scipy.stats.pearsonr(actual.as_matrix().flatten(), predicted)[0]
 
 
-    p_n_elecs = np.sort(np.random.choice(range(len(locs)), n_elecs, replace=False))
-
-    with open(os.path.join(synth_dir, 'synthetic_'+ str(i).rjust(2, '0') + '.bo'), 'rb') as handle:
-        bo_actual = pickle.load(handle)
-        bo_sub = se.Brain(data=bo_actual.data.loc[:, p_n_elecs],locs= bo_actual.locs.loc[p_n_elecs])
-
-    locs_inds = range(0, len(locs))
-    unknown_inds = np.sort(list(set(locs_inds) - set(p_n_elecs)))
-
-    model_patients = [p for p in patients if p != i]
-    for m in model_patients:
-
-        p_m_elecs = np.sort(np.random.choice(range(len(locs)), m_elecs, replace=False))
-        #p_m_elecs = np.random.choice(list(unknown_inds), m_elecs, replace=False)
-        with open(os.path.join(synth_dir, 'synthetic_' + str(m).rjust(2, '0') + '.bo'), 'rb') as handle:
-            bo = pickle.load(handle)
-            #model_data.append(se.Brain(data=bo.data.loc[:, unknown_inds], locs=bo.locs.loc[unknown_inds]))
-            model_data.append(se.Brain(data=bo.data.loc[:, p_m_elecs], locs=bo.locs.loc[p_m_elecs]))
-
-    model = se.Model(data=model_data, locs=bo_actual.locs.loc[unknown_inds])
-    #model = se.Model(data=model_data, locs=locs)
-    reconstructed = model.predict(bo_sub)
-
-
-    new_pd = bo_actual.data.loc[:, unknown_inds]
-    actual = (new_pd - new_pd.mean()) / new_pd.std()
-    predicted = reconstructed.data.loc[:, unknown_inds]
-    corr = np.mean(np.diag(pd.concat([actual, predicted], axis=1, keys=['actual', 'predicted']).corr().loc['actual', 'predicted']))
-
-    d.append({'Patients': m_patients, 'Model Locations': m_elecs, 'Patient Locations': n_elecs, 'Correlation': corr})
+        d.append({'Patients': m_patients, 'Model Locations': m_elecs, 'Patient Locations': n_elecs, 'Correlation': corr})
 
 pd.DataFrame(d)
 
