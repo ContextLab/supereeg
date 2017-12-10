c69106868e5c5d90f8b73912be25789519c9cd5c
Lucy Owen <lucywowen@gmail.com>
2017-12-10 14:07:21 -0500

    issue with installing requirements on cluster

--- cluster/union_locs/union_locs.py
+++ cluster/union_locs/union_locs.py
@@ -22,9 +22,12 @@ data_dir = config['datadir']
 
 bo_files = glob.glob(os.path.join(data_dir,'*.bo'))
 
+
+
 union_locs = pd.DataFrame()
 for b in bo_files:
     bo = se.load(b)
+    #bo = se.filter_subj(se.load(b))
     union_locs = union_locs.append(bo.locs, ignore_index=True)
 
 
