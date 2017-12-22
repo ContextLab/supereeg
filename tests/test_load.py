a5b65e9688158508a52d6f7a1a5c7f4d5fcfde52
Lucy Owen <lucywowen@gmail.com>
2017-12-22 14:07:44 -0500

    working on tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,6 +1,16 @@
 import pytest
 import superEEG as se
 import numpy as np
+import os
+
+data = np.random.multivariate_normal(np.zeros(10), np.eye(10), size=100)
+locs = np.random.multivariate_normal(np.zeros(3), np.eye(3), size=10)
+test_bo = se.Brain(data=data, locs=locs)
+# with open(filepath + '.bo', 'wb') as f:
+#     pickle.dump(self, f)
+#     print('Brain object saved as pickle.')
+
+# update these tests for the new loaded data
 
 def test_load_example_data():
     bo = se.load('example_data')
@@ -17,3 +27,26 @@ def test_load_example_locations():
 def test_load_nifti():
     bo = se.load('example_nifti')
     assert isinstance(bo, se.Brain)
+
+def test_load_pyFR_k10r20_8mm():
+    bo = se.load('pyFR_k10r20_8mm')
+    assert isinstance(bo, se.Model)
+
+def test_load_pyFR_union():
+    locs = se.load('pyFR_union')
+    assert isinstance(locs, np.ndarray)
+
+def test_load_mini_model():
+    bo = se.load('mini_model')
+    assert isinstance(bo, se.Brain)
+
+def test_load_gray_mask_6mm_brain():
+    bo = se.load('gray_mask_6mm_brain')
+    assert isinstance(bo, se.Brain)
+
+
+def test_bo_load(tmpdir):
+    p = tmpdir.mkdir("sub").join("bo")
+    p.write(test_bo)
+    assert p.read() == "bo"
+    assert len(tmpdir.listdir()) == 1
---
+++ tests/test_simulate.py
@@ -0,0 +1,7 @@
+import pytest
+import superEEG as se
+import numpy as np
+
+# clean up simulate.py and write functions that return expected objects
+
+#
