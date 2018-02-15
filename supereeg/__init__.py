b481a2d502038d2aff6efa8e51e6c871f90111a8
lucy <lucywowen@gmail.com>
2018-02-15 17:10:36 -0500

    remove load_nifti for users

--- supereeg/__init__.py
+++ supereeg/__init__.py
@@ -1,6 +1,6 @@
 from .brain import Brain
 from .model import Model
-from .load import load, load_nifti
+from .load import load
 from .simulate import *
 from .helpers import filter_subj, filter_elecs, model_compile, sort_unique_locs, tal2mni, make_gif_pngs
 
