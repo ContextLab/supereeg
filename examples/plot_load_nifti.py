c8ef66a1ce6cd903d14335039df8396b61c5a04e
lucy <lucywowen@gmail.com>
2018-02-20 18:23:39 -0500

    set up debugging to_nii issues

--- examples/plot_load_nifti.py
+++ examples/plot_load_nifti.py
@@ -20,12 +20,26 @@ import nibabel as nib
 import os
 import hypertools as hyp
 
+
+### these 4 cases should all be the same:
+
 # load nifti -> brain object
 bo = se.load('gray_mask_6mm_brain')
 
 # export brain object -> nifti
-nifti = bo.to_nii(vox_size=6)
+nifti_a = bo.to_nii()
+ni_plt.plot_glass_brain(nifti_a)
+
+# export brain object -> nifti
+nifti_b = bo.to_nii(vox_size=6)
+ni_plt.plot_glass_brain(nifti_b)
+
+nifti_c = bo.to_nii(vox_size=[6, 6, 6])
+ni_plt.plot_glass_brain(nifti_c)
+
+nifti_d = bo.to_nii(_gray([6, 6, 6]))
+ni_plt.plot_glass_brain(nifti_d)
 
 # plot the result
-ni_plt.plot_anat(nifti)
+ni_plt.plot_anat(nifti_a)
 ni_plt.show()
