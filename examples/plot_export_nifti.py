00421add6a2a7090a1e4ceb377187b5c1a8540b5
lucy <lucywowen@gmail.com>
2018-03-01 18:09:37 -0500

    working on examples

--- examples/plot_export_nifti.py
+++ examples/plot_export_nifti.py
@@ -25,11 +25,11 @@ bo = se.load('example_data')
 # If no parameters are passed:
 # default uses gray matter masked brain downsampled to 6 mm resolution
 
-# 1: convert with brain object method
-nii_bo1 = bo.to_nii(template='gray', vox_size=20)
+# # 1: convert with brain object method
+# nii_bo1 = bo.to_nii(template='gray', vox_size=20)
 
 # 2: pass to initialize nifti
-nii_bo2 = se.Nifti(bo, template='gray', vox_size=20)
+nii_bo = se.Nifti(bo, template='gray', vox_size=20)
 
 # save nifti
-#nii_bo2.save('/path/to/save/nifti')
+#nii_bo.save('/path/to/save/nifti')
