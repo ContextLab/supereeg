de643ccc45d689039bd92d933aa27cb1f85fd668
andrewheusser <andrew.heusser@gmail.com>
2018-04-04 15:57:23 -0400

    advanced version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LICENSE = 'MIT'
 
 setup(
     name='supereeg',
-    version='0.1.0',
+    version='0.1.1',
     description=DESCRIPTION,
     long_description=' ',
     author='Contextual Dynamics Laboratory',
