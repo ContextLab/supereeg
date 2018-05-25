36811c7fd38ac4e2e784cd8a9e9a3cd88494af82
andrewheusser <andrew.heusser@gmail.com>
2018-05-24 21:55:00 -0400

    fixed

--- supereeg/model.py
+++ supereeg/model.py
@@ -19,7 +19,6 @@ from scipy.spatial.distance import cdist
 class Model(object):
     """
     supereeg model and associated locations
-
     This class holds your supereeg model.  To create an instance, pass a list
     of brain objects and the model will be generated from those brain objects.
     You can also add your own model by passing a numpy array as your matrix and
@@ -27,69 +26,50 @@ class Model(object):
     new model by passing numerator, denominator, locations, and n_subs
     (see parameters for details).  Additionally, you can include a meta dictionary
     with any other information that you want to save with the model.
-
     Parameters
     ----------
-
     data : supereeg.Brain or list supereeg.Brain, supereeg.Nifti or list supereeg.Nifti, or Numpy.ndarray
-
         A supereeg.Brain object or supereeg.Nifti object,  list of objects, or a Numpy.ndarray of your model.
-
     locs : pandas.DataFrame or np.ndarray
         MNI coordinate (x,y,z) by number of electrode df containing electrode locations
-
     template : filepath
         Path to a template nifti file used to set model locations
-
     numerator : Numpy.ndarray
         (Optional) A locations x locations matrix comprising the sum of the log z-transformed
         correlation matrices over subjects.  If used, must also pass denominator,
         locs and n_subs. Otherwise, numerator will be computed from the brain
         object data.
-
     denominator : Numpy.ndarray
         (Optional) A locations x locations matrix comprising the sum of the log (weighted) number of
         subjects contributing to each matrix cell. If used, must also pass numerator,
         locs and n_subs. Otherwise, denominator will be computed from the brain
         object data.
-
     n_subs : int
         The number of subjects used to create the model.  Required if you pass
         numerator/denominator.  Otherwise computed automatically from the data.
-
     rbf_width : positive scalar
         The width of of the radial basis function (RBF) used as a spatial prior for
         smoothing estimates at nearby locations.  (Default: 20)
-
     meta : dict
         Optional dict containing whatever you want
-
     date created : str
         Time created
-
     save : None
         Optional filename to save created model
-
-
     Attributes
     ----------
     numerator : Numpy.ndarray
         A locations x locations matrix comprising the sum of the log z-transformed
         correlation matrices over subjects
-
     denominator : Numpy.ndarray
         A locations x locations matrix comprising the log sum of the (weighted) number of
         subjects contributing to each matrix cell
-
     n_subs : int
         Number of subject used to create the model
-
-
     Returns
     ----------
     model : supereeg.Model instance
         A model that can be used to infer timeseries from unknown locations
-
     """
     def __init__(self, data=None, locs=None, template=None,
                  numerator=None, denominator=None,
@@ -284,41 +264,32 @@ class Model(object):
         Takes a brain object and a 'full' covariance model, fills in all
         electrode timeseries for all missing locations and returns the new brain
         object
-
         Parameters
         ----------
         bo : a Brain, Nifti, or Model object that will be converted to a Brain object.
-
         nearest_neighbor : True
             Default finds the nearest voxel for each subject's electrode
             location and uses that as revised electrodes location matrix in the
             prediction.
-
         match_threshold : 'auto' or int
             auto: if match_threshold auto, ignore all electrodes whose distance
             from the nearest matching voxel is greater than the maximum voxel
             dimension
-
             If value is greater than 0, inlcudes only electrodes that are within
             that distance of matched voxel
-
         force_update : False
             If True, will update model with patient's correlation matrix.
-
         update_locs : True
             If True, and if force_update = False, update the locations in the model to include the locations in the
             given brain object prior to generating the predictions.  If force_update = True, this parameter is
             forced to be True (force_update requires updating the locations) and the specified value is ignored.
-
         preprocess : 'zscore' or None
             The predict algorithm requires the data to be zscored.  However, if
             your data are already zscored you can bypass this by setting to None.
-
         Returns
         ----------
         bo_p : supereeg.Brain
             New brain data object with missing electrode locations filled in
-
         """
 
         if not isinstance(bo, Brain):
@@ -379,25 +350,20 @@ class Model(object):
     def update(self, data, inplace=True):
         """
         Update a model with new data.
-
         Parameters
         ----------
         data : supereeg.Brain, supereeg.Nifti, supereeg.Model (or a mixed list of these)
             New data
-
         inplace : bool
             Whether to run update in place or return a new model (default True).
-
         Returns
         ----------
         model : supereeg.Model
             A new updated model object
-
         """
         if inplace:
             m1 = self
         else:
-
             m1 = Model(self)
 
         m2 = Model(data)
@@ -436,7 +402,6 @@ class Model(object):
     def info(self):
         """
         Print info about the model object
-
         Prints the number of electrodes, number of subjects, date created,
         and any optional meta data.
         """
@@ -445,26 +410,21 @@ class Model(object):
         print('RBF width: ' + str(self.rbf_width))
         print('Date created: ' + str(self.date_created))
         print('Meta data: ' + str(self.meta))
-        print('Radius: ' + str(self.radius))
 
     def plot_data(self, savefile=None, show=True, **kwargs):
         """
         Plot the supereeg model as a correlation matrix
-
         This function wraps seaborn's heatmap and accepts any inputs that seaborn
         supports for models less than 2000x2000.  If the model is larger, the plot cannot be
         generated without specifying a savefile.
-
         Parameters
         ----------
         show : bool
             If False, image not rendered (default : True)
-
         Returns
         ----------
         ax : matplotlib.Axes
             An axes object
-
         """
 
         corr_mat = self.get_model(z_transform=False)
@@ -484,13 +444,10 @@ class Model(object):
     def plot_locs(self, pdfpath=None):
         """
         Plots electrode locations from brain object
-
-
         Parameters
         ----------
         pdfpath : str
         A name for the file.  If the file extension (.pdf) is not specified, it will be appended.
-
         """
 
         locs = self.locs
@@ -502,21 +459,17 @@ class Model(object):
     def save(self, fname, compression='blosc'):
         """
         Save method for the model object
-
         The data will be saved as a 'mo' file, which is a dictionary containing
         the elements of a model object saved in the hd5 format using
         `deepdish`.
-
         Parameters
         ----------
         fname : str
             A name for the file.  If the file extension (.mo) is not specified,
             it will be appended.
-
         compression : str
             The kind of compression to use.  See the deepdish documentation for
             options: http://deepdish.readthedocs.io/en/latest/api_io.html#deepdish.io.save
-
         """
 
         mo = {
@@ -537,16 +490,13 @@ class Model(object):
     def get_slice(self, inds, inplace=False):
         """
         Indexes model object data
-
         Parameters
         ----------
         inds : scalar, list, or numpy array
             locations you wish to index (relative to model.get_locs())
-
         inplace : bool
             If True, indexes in place; otherwise a new Model object is returned
             (default: False)
-
         """
         numerator = self.numerator[inds][:, inds]
         denominator = self.denominator[inds][:, inds]
@@ -554,7 +504,6 @@ class Model(object):
         n_subs = self.n_subs
         meta = self.meta
         date_created = time.strftime("%c")
-        radius = self.radius
 
         if inplace:
             self.numerator = numerator
@@ -563,18 +512,15 @@ class Model(object):
             self.n_subs = n_subs
             self.meta = meta
             self.date_created = date_created
-            self.radius = radius
         else:
             return Model(numerator=numerator, denominator=denominator, locs=locs,
                          n_subs=n_subs, meta=meta, date_created=date_created, rbf_width=self.rbf_width)
-
 
     def __add__(self, other):
         """
         Add two model objects together. The models must have matching
         locations.  Meta properties are combined across objects, or if properties
         conflict then the values from the first object are preferred.
-
         Parameters
         ----------
         other: Model object to be added to the current object
@@ -626,7 +572,7 @@ class Model(object):
 # helper functions for init
 ###################################
 
-def _handle_superuser(self, numerator, denominator, locs, n_subs, radius):
+def _handle_superuser(self, numerator, denominator, locs, n_subs):
     """Shortcuts model building if these args are passed"""
     self.numerator = numerator
     self.denominator = denominator
@@ -638,7 +584,6 @@ def _handle_superuser(self, numerator, denominator, locs, n_subs, radius):
         self.locs = pd.DataFrame(locs, columns=['x', 'y', 'z'])
 
     self.n_subs = n_subs
-    self.radius = radius
 
 def _create_locs(self, locs, template):
     """get locations from template, or from locs arg"""
@@ -677,7 +622,6 @@ def _mo2model(mo, locs, width=20):
         return n, d, mo.n_subs
 
 def _force_update(mo, bo, width=20):
-
     # get subject-specific correlation matrix
     sub_corrmat = _get_corrmat(bo)
 
@@ -689,7 +633,6 @@ def _force_update(mo, bo, width=20):
 
     # get _rbf weights
     sub__rbf_weights = _log_rbf(mo.locs, bo.get_locs(), width=width)
-
 
     #  get subject expanded correlation matrix
     num_corrmat_x, denom_corrmat_x = _blur_corrmat(sub_corrmat_z, sub__rbf_weights)
@@ -815,107 +758,3 @@ def _recover_model(num, denom, z_transform=False):
     else:
         np.fill_diagonal(m, 1)
         return _z2r(m)
-    with np.errstate(invalid='ignore'):
-        model_corrmat_x = np.divide(np.add(mo.numerator, num_corrmat_x), np.add(mo.denominator, denom_corrmat_x))
-
-    return model_corrmat_x
-
-###################################
-# helper functions for predict
-###################################
-
-def _which_case(bo, bool_mask):
-    """Determine which predict scenario we are in"""
-    if all(bool_mask):
-        return 'all_overlap'
-    if not any(bool_mask):
-        return 'no_overlap'
-    elif sum(bool_mask) == bo.get_locs().shape[0]:
-        return 'subset'
-    elif sum(bool_mask) != bo.get_locs().shape[0]:
-        return 'some_overlap'
-
-
-def _no_overlap(self, bo, model_corrmat_x):
-    """ Compute model when there is no overlap """
-    # expanded _rbf weights
-    model__rbf_weights = _rbf(pd.concat([self.locs, bo.get_locs()]), self.locs, self.radius)
-
-    # get model expanded correlation matrix
-    num_corrmat_x, denom_corrmat_x = _expand_corrmat_predict(model_corrmat_x, model__rbf_weights)
-
-    # divide the numerator and denominator
-    with np.errstate(invalid='ignore'):
-        model_corrmat_x = np.divide(num_corrmat_x, denom_corrmat_x)
-
-    # label locations as reconstructed or observed
-    loc_label = ['reconstructed'] * len(self.locs) + ['observed'] * len(bo.get_locs())
-
-    # grab the locs
-    perm_locs = self.locs.append(bo.get_locs())
-
-    return model_corrmat_x, loc_label, perm_locs
-
-def _subset(self, bo, model_corrmat_x, joint_model_inds):
-    """ Compute model when bo is a subset of the model """
-    # permute the correlation matrix so that the inds to reconstruct are on the right edge of the matrix
-    perm_inds = sorted(set(range(self.locs.shape[0])) - set(joint_model_inds)) + sorted(set(joint_model_inds))
-    model_corrmat_x = model_corrmat_x[:, perm_inds][perm_inds, :]
-
-    # label locations as reconstructed or observed
-    loc_label = ['reconstructed'] * (len(self.locs)-len(bo.get_locs())) + ['observed'] * len(bo.get_locs())
-
-    # grab permuted locations
-    perm_locs = self.locs.iloc[perm_inds]
-
-    return model_corrmat_x, loc_label, perm_locs
-
-def _some_overlap(self, bo, model_corrmat_x, joint_model_inds):
-    """ Compute model when there is some overlap """
-
-    # get subject indices where subject locs do not overlap with model locs
-
-    bool_bo_mask= np.sum([(bo.get_locs() == y).all(1) for idy, y in self.locs.iterrows()], 0).astype(bool)
-    disjoint_bo_inds = np.where(~bool_bo_mask)[0]
-    # d = cdist(bo.get_locs(), self.locs)
-    # disjoint_bo_inds = np.where(np.isclose(d, 0))[0]
-
-    # permute the correlation matrix so that the inds to reconstruct are on the right edge of the matrix
-    perm_inds = sorted(set(range(self.locs.shape[0])) - set(joint_model_inds)) + sorted(set(joint_model_inds))
-    model_permuted = model_corrmat_x[:, perm_inds][perm_inds, :]
-
-    # permute the model locations (important for the _rbf calculation later)
-    model_locs_permuted = self.locs.iloc[perm_inds]
-
-    # permute the subject locations arranging them
-    bo_perm_inds = sorted(set(range(bo.get_locs().shape[0])) - set(disjoint_bo_inds)) + sorted(set(disjoint_bo_inds))
-    sub_bo = bo.get_locs().iloc[disjoint_bo_inds]
-
-    #TODO: would be safer to implement this using bo.get_locs(), bo.get_data()
-    bo.locs = bo.locs.iloc[bo_perm_inds]
-    bo.data = bo.data[bo_perm_inds]
-    bo.kurtosis = bo.kurtosis[bo_perm_inds]
-
-    # permuted indices for unknown model locations
-    perm_inds_unknown = sorted(set(range(self.locs.shape[0])) - set(joint_model_inds))
-    # expanded _rbf weights
-    #model__rbf_weights = _rbf(pd.concat([model_locs_permuted, bo.locs]), model_locs_permuted)
-    model__rbf_weights = _rbf(pd.concat([model_locs_permuted, sub_bo]), model_locs_permuted, self.radius)
-
-    # get model expanded correlation matrix
-    num_corrmat_x, denom_corrmat_x = _expand_corrmat_predict(model_permuted, model__rbf_weights)
-
-    # divide the numerator and denominator
-    with np.errstate(invalid='ignore'):
-        model_corrmat_x = np.divide(num_corrmat_x, denom_corrmat_x)
-
-    # add back the permuted correlation matrix for complete subject prediction
-    model_corrmat_x[:model_permuted.shape[0], :model_permuted.shape[0]] = model_permuted
-
-    # label locations as reconstructed or observed
-    loc_label = ['reconstructed'] * len(self.locs.iloc[perm_inds_unknown]) + ['observed'] * len(bo.get_locs())
-
-    ## unclear if this will return too many locations
-    perm_locs = self.locs.iloc[perm_inds_unknown].append(bo.get_locs())
-
-    return model_corrmat_x, loc_label, perm_locs
