35afad61dd74ace92dadbd47379064ee88c9fa7e
Jeremy Manning <jeremy@hypercube.local>
2018-07-27 23:37:56 -0400

    got rid of warning message due to harmless numpy/cython issues

--- supereeg/__init__.py
+++ supereeg/__init__.py
@@ -1,3 +1,8 @@
+#deal with numpy import warnings due to cython (https://stackoverflow.com/questions/40845304/runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility)
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 from .brain import Brain
 from .model import Model
 from .nifti import Nifti
