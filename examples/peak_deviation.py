5ae3816cb53beb07192e8b757968e620c61ad443
tmuntianu <tmuntia@yahoo.com>
2019-04-12 23:34:00 -0400

    fixed some stuff

--- examples/peak_deviation.py
+++ examples/peak_deviation.py
@@ -11,10 +11,9 @@ import sklearn
 
 # if fname != '':
 #     bo = se.load(fname)
-# else:
-    # simulate bo
-locs = se.simulate_locations(n_elecs=3)
-bo = se.simulate_bo(n_samples=3000, sample_rate=200, cov='random', locs=locs, noise =.1)
+
+
+bo = se.load('P0010sz1.bo')
 
 freqs = np.logspace(np.log10(2), np.log10(100))
 delta = freqs[0:8]
@@ -28,32 +27,30 @@ hgamma = freqs[42:50]
 for time in list(range(0, 1)): #len(z_power[0])))[::1000]:
     wav_transform, sj, freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[0][time:time+1000], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
     raw_power = np.square(np.abs(wav_transform))
-    log_power = np.log(raw_power)
-    z_power = np.apply_over_axes(scipy.stats.mstats.zscore, raw_power, 1)
+    avg_power = np.average(raw_power, axis=1)
+    plt.figure()
+    plt.plot(freqs, avg_power)
     for electrode in range(1, len(bo.data.T)):
         transformed, sj, freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode][time:time+1000], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
-        raw_power2 = np.square(np.abs(transformed))
-        log_power2 = np.log(raw_power2)
-        z_power2 = np.apply_over_axes(scipy.stats.mstats.zscore, raw_power2, 1)
-        z_power += z_power2
-    z_power /= len(bo.data.T)
+        avg_power += np.average(np.square(np.abs(transformed)), axis=1)
+    avg_power /= len(bo.data.T)
+    log_power = np.log(avg_power)
+    print(log_power.shape)
+    z_power = scipy.stats.mstats.zscore(log_power)
     print(z_power.shape)
     HR = sklearn.linear_model.HuberRegressor()
-    avg_power = np.average(z_power, axis=1)
-    HR.fit(freqs.reshape(-1,1), avg_power)
-    broadband_power = avg_power - (freqs * HR.coef_[0] + HR.intercept_)
+    HR.fit(freqs.reshape(-1,1), z_power)
+    narrowband_power = z_power - (freqs * HR.coef_[0] + HR.intercept_)
     xs = np.linspace(0, 100, 500)
     ys = HR.coef_[0] * xs + HR.intercept_
-    peak_deviation = np.amax(broadband_power)
+    peak_deviation = np.amax(narrowband_power)
+
+
 plt.figure()
-plt.plot(xs,ys)
-plt.plot(freqs, avg_power)
-plt.plot(freqs, broadband_power)
-
-
-
-# for i in range(0, len(transformed)):
-#     plt.figure()
-#     plt.plot(np.arange(0,len(transformed[i])), transformed[i])
-#     plt.title('freq ' + str(i))
-# plt.show()
+plt.plot(xs,ys, label='broad')
+plt.plot(freqs, avg_power, label='avg')
+plt.plot(freqs, narrowband_power, label='narrow')
+plt.legend(loc='upper right')
+plt.figure()
+plt.plot(freqs, raw_power)
+plt.show()
