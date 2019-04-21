4978aacd20e3e4bfcba49436e8b1ced39c8c1c46
tmuntianu <tmuntia@yahoo.com>
2019-04-21 03:12:05 -0400

    finalized peak_deviation, peak_deviation by epoch, and power scripts

--- examples/peak_deviation.py
+++ examples/peak_deviation.py
@@ -21,23 +21,20 @@ try:
     hgamma = freqs[42:50]
 
     epoch = 1000
+    log_freqs = np.log(freqs)
 
     peak_deviations = np.zeros(shape=bo.data.T.shape)
 
-    toprange = int(np.floor(len(bo.data[0])/epoch)*epoch)
-    for time in list(range(0, toprange))[::epoch]:
-        for electrode in range(0, len(bo.data.T)):
-            wav_transform, sj, wavelet_freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode][time:time+epoch], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
-            raw_power = np.square(np.abs(wav_transform))
-            avg_power = np.average(raw_power, axis=1)
-            log_power = np.log(avg_power)
-            log_freqs = np.log(freqs)
-            HR = sklearn.linear_model.HuberRegressor()
-            HR.fit(log_freqs.reshape(-1,1), log_power)
-            narrowband_power = log_power - (log_freqs * HR.coef_[0] + HR.intercept_)
-            peak_deviations[electrode][time:time+epoch] = narrowband_power
-        print('time ' + str(time) + ' of ' + str(toprange))
-
+    for electrode in range(0, len(bo.data.T)):
+        wav_transform, sj, wavelet_freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode][time:time+epoch], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
+        raw_power = np.square(np.abs(wav_transform))
+        avg_power = np.average(raw_power, axis=0)
+        log_power = np.log(avg_power)
+        
+        HR = sklearn.linear_model.HuberRegressor()
+        HR.fit(log_freqs.reshape(-1,1), log_power)
+        narrowband_power = log_power - (log_freqs * HR.coef_[0] + HR.intercept_)
+        peak_deviations[electrode] = narrowband_power
 
     deviation_bo = se.Brain(data=peak_deviations.T, locs=bo.locs, sample_rate=bo.sample_rate, filter=None)
     deviation_bo.save('peakdev_' + fname)
