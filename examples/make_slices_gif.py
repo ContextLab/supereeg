92bf6f99b6c31e4fc80424aab9df21b383a741b5
tmuntianu <tmuntia@yahoo.com>
2019-04-21 02:17:28 -0400

    added peakdeviation, power scripts, made changes to slice gif script

--- examples/make_slices_gif.py
+++ examples/make_slices_gif.py
@@ -1,35 +1,21 @@
 # -*- coding: utf-8 -*-
-"""
-=============================
-Make gif
-=============================
-
-In this example, we load in a single subject example, remove electrodes that exceed
-a kurtosis threshold (in place), load a model, and predict activity at all
-model locations.  We then convert the reconstruction to a nifti and plot 3 consecutive timepoints
-first with the plot_glass_brain and then create .png files and compile as a gif.
-
-"""
-
-# Code source: Lucy Owen & Andrew Heusser, modified by Tudor Muntianu
-# License: MIT
 
 # load
 import supereeg as se
 import numpy as np
 
 # load example data
-# bo = se.load('example_data')
+bo = se.load('peakdev.bo')
 
 # simulate 100 locations
-locs = se.simulate_locations(n_elecs=100)
+# locs = se.simulate_locations(n_elecs=100)
 
 # simulate brain object
-bo = se.simulate_bo(n_samples=400, sample_rate=100, cov='random', locs=locs, noise =.1)
+# bo = se.simulate_bo(n_samples=400, sample_rate=100, cov='random', locs=locs, noise =.1)
 
 # convert to nifti
 nii = bo.to_nii(template='std', vox_size=6)
 
 # make gif
 # '/your/path/to/gif/'
-nii.make_sliced_gif('C:\\Users\\tmunt\\Documents\\gif', time_index=np.arange(10), slice_index=range(-4,52,4), name='sample_gif', alpha=0.7)
+nii.make_sliced_gif('C:\\Users\\tmunt\\Documents\\gif', time_index=np.arange(len(bo.data[0])), slice_index=range(-4,52,4), name='sample_gif', vmax=3, symmetric_cbar=False, duration=200, alpha=0.7)
--- examples/peak_deviation.py
+++ examples/peak_deviation.py
@@ -6,64 +6,41 @@ import pycwt as wavelet
 import matplotlib.pyplot as plt
 import sklearn
 
-# if __name__ == "__main__":
-#     fname = sys.argv[0]
+if __name__ == "__main__":
+    fname = sys.argv[0]
 
-# if fname != '':
-#     bo = se.load(fname)
+try:
+    bo = se.load(fname)
 
-bo = se.load('P0010sz1_seizure.bo')
-# bo = se.load('example_data')
-# locs = se.simulate_locations(n_elecs=10)
-# bo = se.simulate_bo(n_samples=3000, sample_rate=200, cov='random', locs=locs, noise =.1)
+    freqs = np.logspace(np.log10(2), np.log10(100))
+    delta = freqs[0:8]
+    theta = freqs[8:17]
+    alpha = freqs[17:22]
+    beta = freqs[22:33]
+    lgamma = freqs[33:42]
+    hgamma = freqs[42:50]
 
-freqs = np.logspace(np.log10(2), np.log10(100))
-delta = freqs[0:8]
-theta = freqs[8:17]
-alpha = freqs[17:22]
-beta = freqs[22:33]
-lgamma = freqs[33:42]
-hgamma = freqs[42:50]
+    epoch = 1000
 
-epoch = 100
+    peak_deviations = np.zeros(shape=bo.data.T.shape)
 
-time_points, num_electrodes = bo.data.values.shape
-peak_deviations = []
-
-for time in list(range(0, len(bo.data[0])))[0:1000:epoch]:
-    wav_transform, sj, freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[0][time:time+epoch], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
-    raw_power = np.square(np.abs(wav_transform))
-    avg_power = np.average(raw_power, axis=1)
-    for electrode in range(1, len(bo.data.T)):
-        transformed, sj, freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode][time:time+epoch], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
-        avg_power += np.average(np.square(np.abs(transformed)), axis=1)
-    
-    avg_power /= len(bo.data.T)
-    log_power = np.log(avg_power)
-    z_power = scipy.stats.mstats.zscore(log_power)
-    log_freqs = np.log(freqs)
-    HR = sklearn.linear_model.HuberRegressor()
-    HR.fit(log_freqs.reshape(-1,1), log_power)
-    narrowband_power = log_power - (log_freqs * HR.coef_[0] + HR.intercept_)
-    xs = np.linspace(0, 5, 100)
-    ys = HR.coef_[0] * xs + HR.intercept_
-    peak_deviation = np.amax(narrowband_power)
-    peak_deviations.append(peak_deviation)
-    plt.figure()
-    plt.plot(xs,ys, label='broad')
-    plt.plot(log_freqs, log_power, label='log')
-    plt.plot(log_freqs, narrowband_power, label='narrow')
+    toprange = int(np.floor(len(bo.data[0])/epoch)*epoch)
+    for time in list(range(0, toprange))[::epoch]:
+        for electrode in range(0, len(bo.data.T)):
+            wav_transform, sj, wavelet_freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode][time:time+epoch], 1/bo.sample_rate[0], freqs = freqs, wavelet=wavelet.Morlet(4))
+            raw_power = np.square(np.abs(wav_transform))
+            avg_power = np.average(raw_power, axis=1)
+            log_power = np.log(avg_power)
+            log_freqs = np.log(freqs)
+            HR = sklearn.linear_model.HuberRegressor()
+            HR.fit(log_freqs.reshape(-1,1), log_power)
+            narrowband_power = log_power - (log_freqs * HR.coef_[0] + HR.intercept_)
+            peak_deviations[electrode][time:time+epoch] = narrowband_power
+        print('time ' + str(time) + ' of ' + str(toprange))
 
 
-deviation_bo = se.Brain(data=peak_deviations, locs=bo.locs, sample_rate=bo.sample_rate)
-deviation_bo.plot_data()
-deviation_bo.plot_locs()
-# bo.plot_data()
-# plt.figure()
-# plt.plot(xs,ys, label='broad')
-# plt.plot(log_freqs, log_power, label='log')
-# plt.plot(log_freqs, narrowband_power, label='narrow')
-# plt.legend(loc='upper right')
-# plt.figure()
-# plt.plot(freqs, raw_power)
-plt.show()
+    deviation_bo = se.Brain(data=peak_deviations.T, locs=bo.locs, sample_rate=bo.sample_rate, filter=None)
+    deviation_bo.save('peakdev_' + fname)
+
+except:
+    print('.bo file not found')
