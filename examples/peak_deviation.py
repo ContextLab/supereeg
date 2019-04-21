861a1145b1f753ddd9bde85e95350a9ee2f8d87a
tmuntianu <tmuntia@yahoo.com>
2019-04-21 17:11:09 -0400

    remove try except

--- examples/peak_deviation.py
+++ examples/peak_deviation.py
@@ -8,36 +8,36 @@ import sklearn
 if __name__ == "__main__":
     fname = sys.argv[1]
 
-try:
-    bo = se.load(fname)
-    
-    freqs = np.logspace(np.log10(2), np.log10(100))
-    delta = freqs[0:8]
-    theta = freqs[8:17]
-    alpha = freqs[17:22]
-    beta = freqs[22:33]
-    lgamma = freqs[33:42]
-    hgamma = freqs[42:50]
+# try:
+bo = se.load(fname)
 
-    bands = [delta, theta, alpha, beta, lgamma, hgamma]
+freqs = np.logspace(np.log10(2), np.log10(100))
+delta = freqs[0:8]
+theta = freqs[8:17]
+alpha = freqs[17:22]
+beta = freqs[22:33]
+lgamma = freqs[33:42]
+hgamma = freqs[42:50]
 
-    log_freqs = np.log(freqs)
+bands = [delta, theta, alpha, beta, lgamma, hgamma]
 
-    peak_deviations = np.zeros(shape=bo.data.T.shape)
+log_freqs = np.log(freqs)
 
-    for i, band in enumerate(bands):
-        for electrode in range(0, len(bo.data.T)):
-            wav_transform, sj, wavelet_freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode], 1/bo.sample_rate[0], freqs = band, wavelet=wavelet.Morlet(4))
-            raw_power = np.square(np.abs(wav_transform))
-            avg_power = np.average(raw_power, axis=0)
-            log_power = np.log(avg_power)
-            HR = sklearn.linear_model.HuberRegressor()
-            HR.fit(log_freqs.reshape(-1,1), log_power)
-            narrowband_power = log_power - (log_freqs * HR.coef_[0] + HR.intercept_)
-            peak_deviations[electrode] = narrowband_power
+peak_deviations = np.zeros(shape=bo.data.T.shape)
 
-        deviation_bo = se.Brain(data=peak_deviations.T, locs=bo.locs, sample_rate=bo.sample_rate, filter=None)
-        deviation_bo.save('peakdev_band_' + str(i) + '_' + fname)
+for i, band in enumerate(bands):
+    for electrode in range(0, len(bo.data.T)):
+        wav_transform, sj, wavelet_freqs, coi, fft, fftfreqs = wavelet.cwt(bo.data[electrode], 1/bo.sample_rate[0], freqs = band, wavelet=wavelet.Morlet(4))
+        raw_power = np.square(np.abs(wav_transform))
+        avg_power = np.average(raw_power, axis=0)
+        log_power = np.log(avg_power)
+        HR = sklearn.linear_model.HuberRegressor()
+        HR.fit(log_freqs.reshape(-1,1), log_power)
+        narrowband_power = log_power - (log_freqs * HR.coef_[0] + HR.intercept_)
+        peak_deviations[electrode] = narrowband_power
 
-except:
-    print('.bo file not found')
+    deviation_bo = se.Brain(data=peak_deviations.T, locs=bo.locs, sample_rate=bo.sample_rate, filter=None)
+    deviation_bo.save('peakdev_band_' + str(i) + '_' + fname)
+
+# except:
+#     print('.bo file not found')
