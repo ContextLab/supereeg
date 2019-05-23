343717982776c12171157d1d3b301ba7bd4bf57c
tmuntianu <tmuntia@yahoo.com>
2019-05-23 02:00:20 -0400

    voxel size changes

--- examples/parallel_gif.py
+++ examples/parallel_gif.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     print(fname)
     timepoints = bo.data.shape[0]
     ranges = np.array_split(np.arange(timepoints), nworkers) #nworkers
-    nii = bo.to_nii2()
+    nii = bo.to_nii2(template='std', vox_size=6)
     path = '/dartfs/rc/lab/D/DBIC/CDL/f003f64/gifs' # 'C:/Users/tmunt/Documents/gif'
     # helpwrap = wrapper(helper, nifti=nii, path=path, slice_index=range(-50, 50, 4), vmax=vmax, symmetric_cbar=True, display_mode='y')
     pr = cProfile.Profile()
